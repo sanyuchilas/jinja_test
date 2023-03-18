f246fe04317ac6f93ae51f28bd63477ffe28dbab
Alexander Keyer <sanyuchilas@gmail.com>
2023-03-18 22:34:09 +0300

    need

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+class Card:
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+cards = [
+    Card('Todo', 'Сделать template'),
+    Card('Done', 'Защитить проект')
+]
+
 @app.route('/')
-def log():
-    return 'log'
+def index():
+    return render_template('index.html.j2',
+        cards=cards,
+        test=True
+    )
+@app.route('/card/', methods=['POST'])
+def create_card():
+    body = request.json
+
+    return ['adafadf', 'aafadf']
